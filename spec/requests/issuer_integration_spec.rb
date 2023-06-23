51e012fbaf099ea6f5594c54508927b8c9f96e56
Nando Sousa <nandosousafr@users.noreply.github.com>
2023-06-23 20:07:50 +0000

    add specs for the issuer transaction

---
+++ spec/requests/issuer_integration_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'Issuer Integration', type: :request do
+  xdescribe '/api/auth_transactions' do
+    context 'when the transaction amount greater than $1k' do
+      it 'declines the transaction' do
+        payload = {
+          amount: 2_000,
+          card_number: "4321123443211234",
+        }
+
+        post '/api/auth_transactions', params: payload
+
+        expect(response.code).to eq 422
+        expect(JSON.parse(response.body)).to eq message: "Not enough funds"
+      end
+    end
+
+    context 'when the transaction amount is less than $1k' do
+      it 'declines the transaction' do
+        payload = {
+          amount: 700,
+          card_number: "4321123443211234",
+        }
+
+        post '/api/auth_transactions', params: payload
+
+        expect(response.code).to eq 201
+        expect(JSON.parse(response.body)).to eq message: "OK"
+      end      
+    end
+
+    context 'when the transaction amount is equal to $1k' do
+      it 'declines the transaction' do
+        payload = {
+          amount: 1000,
+          card_number: "4321123443211234",
+        }
+
+        post '/api/auth_transactions', params: payload
+
+        expect(response.code).to eq 422
+        expect(JSON.parse(response.body)).to eq message: "OK"
+      end      
+    end
+  end
+end
